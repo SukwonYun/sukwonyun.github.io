b8e0e9d08b7f98db0423101a3f0fced320f36934
SukwonYun <tyryey89@naver.com>
2021-01-30 16:05:17 +0900

    Add file

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "[추천시스템] Matrix Factorization Techniques for Recommender Systems",
+        "excerpt":"Koren, Yehuda, Robert Bell, and Chris Volinsky. “Matrix factorization techniques for recommender systems.” Computer 42.8 (2009): 30-37. In short, “Basic models of Matrix Factorizaiton” About 이 논문은 추천 시스템의 갈래, 그리고 Matrix Factorization 의 기초를 다루고 있는 논문으로 향 후 어떠한 모델 등으로 발전해왔는지 서술한 추천 시스템의 기본 배경지식을 잡아주는...","categories": ["RecommenderSystem"],
+        "tags": [],
+        "url": "https://sukwonyun.github.io/recommendersystem/Netflix/",
+        "teaser": null
+      },{
+        "title": "Jekyll 테마에서 LaTex 사용하기",
+        "excerpt":"Github 블로그에 LaTex로 수식 표현하기 1. _config.yml에서 마크다운 확인하기 _config.yml 파일에서 마크다운이 다음과 같이 설정되어 있는지 확인한다. Github 업데이트로 인해 따로 kramdown 이 알아서 $$, 블록처리를 인식해 줄 것이다. 2. _includes 폴더에 Mathjax.html 파일을 만들고 아래 내용을 넣어준다. 3. _layouts 폴더 안 default.html 파일 맨 밑에 Mathjax 를 include 해준다....","categories": ["Jekyll"],
+        "tags": [],
+        "url": "https://sukwonyun.github.io/jekyll/Jekyll-%ED%85%8C%EB%A7%88%EC%97%90%EC%84%9C-Latex-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "[추천시스템] Wide & Deep Learning for Recommender Systems",
+        "excerpt":"Cheng, Heng-Tze, et al. “Wide &amp; deep learning for recommender systems.” Proceedings of the 1st workshop on deep learning for recommender systems. 2016. In short, “Memorization (Wide) + Generalization (Deep)” About 이 논문은 추천시스템의 Memorization, Generalization 의 장점을 결합한 새로운 학습 방법을 제시해주는 논문이다. Abstract Generalization 모델은 적은 수의 인풋을...","categories": ["RecommenderSystem"],
+        "tags": [],
+        "url": "https://sukwonyun.github.io/recommendersystem/WideDeep/",
+        "teaser": null
+      }]
