24a1ee83e5384a22795cf2f70642cfe2af8175d9
sukwonyun <tyryey89@naver.com>
2021-02-18 00:27:50 +0900

    Update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,8 @@
 var store = [{
         "title": "[추천시스템] Matrix Factorization Techniques for Recommender Systems",
-        "excerpt":"Koren, Yehuda, Robert Bell, and Chris Volinsky. “Matrix factorization techniques for recommender systems.” Computer 42.8 (2009): 30-37. In short, “Basic models of Matrix Factorizaiton” About 이 논문은 추천 시스템의 갈래, 그리고 Matrix Factorization 의 기초를 다루고 있는 논문으로 향 후 어떠한 모델 등으로 발전해왔는지 서술한 추천 시스템의 기본 배경지식을 잡아주는...","categories": ["RecommenderSystem"],
+        "excerpt":"Koren, Yehuda, Robert Bell, and Chris Volinsky. “Matrix factorization techniques for recommender systems.” Computer 42.8 (2009): 30-37. In short, “Basic models of Matrix Factorizaiton” About 이 논문은 추천 시스템의 갈래, 그리고 Matrix Factorization 의 기초를 다루고 있는 논문으로 향 후 어떠한 모델 등으로 발전해왔는지 서술한 추천 시스템의 기본 배경지식을 잡아주는...","categories": ["추천시스템"],
         "tags": [],
-        "url": "https://sukwonyun.github.io/recommendersystem/Netflix/",
+        "url": "https://sukwonyun.github.io/%EC%B6%94%EC%B2%9C%EC%8B%9C%EC%8A%A4%ED%85%9C/Netflix/",
         "teaser": null
       },{
         "title": "Jekyll 테마에서 LaTex 사용하기",
@@ -12,14 +12,20 @@ var store = [{
         "teaser": null
       },{
         "title": "[추천시스템] Wide & Deep Learning for Recommender Systems",
-        "excerpt":"Cheng, Heng-Tze, et al. “Wide &amp; deep learning for recommender systems.” Proceedings of the 1st workshop on deep learning for recommender systems. 2016. In short, “Memorization (Wide) + Generalization (Deep)” About 이 논문은 추천시스템의 Memorization, Generalization 의 장점을 결합한 새로운 학습 방법을 제시해주는 논문이다. Abstract Generalization 모델은 적은 수의 인풋을...","categories": ["RecommenderSystem"],
+        "excerpt":"Cheng, Heng-Tze, et al. “Wide &amp; deep learning for recommender systems.” Proceedings of the 1st workshop on deep learning for recommender systems. 2016. In short, “Memorization (Wide) + Generalization (Deep)” About 이 논문은 추천시스템의 Memorization, Generalization 의 장점을 결합한 새로운 학습 방법을 제시해주는 논문이다. Abstract Generalization 모델은 적은 수의 인풋을...","categories": ["추천시스템"],
         "tags": [],
-        "url": "https://sukwonyun.github.io/recommendersystem/WideDeep/",
+        "url": "https://sukwonyun.github.io/%EC%B6%94%EC%B2%9C%EC%8B%9C%EC%8A%A4%ED%85%9C/WideDeep/",
         "teaser": null
       },{
         "title": "[지식그래프] (TransE) Translating Embeddings for Modeling Multi-relational Data",
-        "excerpt":"Bordes, Antoine, et al. “Translating embeddings for modeling multi-relational data.” Neural Information Processing Systems (NIPS). 2013. In short, “Embedding Entities and Relationships in Same space” About 이 논문은 지식 그래프에서 가장 기반이 되는 Translation-based model 이라고 할 수 있고, 저 차원의 벡터 공간에서 가볍고 성능이 좋은 모델을 제시하는데 의의가 있다....","categories": [],
+        "excerpt":"Bordes, Antoine, et al. “Translating embeddings for modeling multi-relational data.” Neural Information Processing Systems (NIPS). 2013. In short, “Embedding Entities and Relationships in Same space” About 이 논문은 지식 그래프에서 가장 기반이 되는 Translation-based model 이라고 할 수 있고, 저 차원의 벡터 공간에서 가볍고 성능이 좋은 모델을 제시하는데 의의가 있다....","categories": ["지식그래프"],
         "tags": [],
-        "url": "https://sukwonyun.github.io/TransE/",
+        "url": "https://sukwonyun.github.io/%EC%A7%80%EC%8B%9D%EA%B7%B8%EB%9E%98%ED%94%84/TransE/",
+        "teaser": null
+      },{
+        "title": "[지식그래프] (TransR) Learning Entity and Relation Embeddings for Knowledge Graph Completion",
+        "excerpt":"Lin, Yankai, et al. “Learning entity and relation embeddings for knowledge graph completion.” Proceedings of the AAAI Conference on Artificial Intelligence. Vol. 29. No. 1. 2015. In short, “Embedding Entities and Relationships in Seperate Entity and Realtion space” About 이 논문은 TransE(2013), TransH(2014)에서 제시된 모델을 개선해 구성요소와 관계가 각각 다른...","categories": ["지식그래프"],
+        "tags": [],
+        "url": "https://sukwonyun.github.io/%EC%A7%80%EC%8B%9D%EA%B7%B8%EB%9E%98%ED%94%84/TransH/",
         "teaser": null
       }]
